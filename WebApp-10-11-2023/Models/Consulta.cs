9f5db8866e226152e2db0271ce502354fe2c1ef4
franciscaschwyzer <147095493+franciscaschwyzer@users.noreply.github.com>
2023-11-10 15:39:33 -0300

    Projeto Integrador

---
+++ WebApp-10-11-2023/Models/Consulta.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp_10_11_2023.Models
+{
+    public class Consulta
+    {
+        [Key]
+
+        public int IdConsulta { get; set; }
+
+        public int IdPaciente { get; set; }
+
+        public string dataConsulta { get; set; }
+
+        public string fichaConsulta { get; set; }
+    }
+}
